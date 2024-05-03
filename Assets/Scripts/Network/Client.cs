9fb9a1ca63c114013f4b65359d383917825f5324
Miguelstucom <114105189+Miguelstucom@users.noreply.github.com>
2024-05-03 16:41:06 +0200

    test

--- Assets/Scripts/Network/Client.cs
+++ Assets/Scripts/Network/Client.cs
@@ -1,62 +1,165 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Networking.Transport;
+using Unity.Collections;
 
 public class Client : MonoBehaviour
 {
     public string ipAddress = "127.0.0.1";
     public ushort port = 9000;
-    public NetworkDriver net_driver;
-    public NetworkConnection conexion;
+public int id_key=-1; //identificador que donar� el servidor
+private Dictionary<int, Player> oponents = new Dictionary<int, Player>();
+Player player;
+public float velocitat = 0.0001f;
+    Vector3 posicio_ultima = new Vector3(0, 0);
+    [SerializeField] Player playerPrefab;
 
-    public GameObject playerPrefab; // Prefab del jugador
-
+    public NetworkDriver network_driver;
+    private NetworkConnection connexio;
+    // Start is called before the first frame update
     void Start()
     {
-        net_driver = NetworkDriver.Create();
-        conexion = default(NetworkConnection);
-        var direccion_servidor = NetworkEndpoint.Parse(ipAddress, port);
-        conexion = net_driver.Connect(direccion_servidor);
-        Debug.Log("CLIENT: Conectando");
-    }
+        string ipAddressCanvas = "127.0.0.1";
+        ushort.TryParse("9000", out var portCanvas);
+        ipAddress = string.IsNullOrEmpty(ipAddressCanvas) ? ipAddress : ipAddressCanvas;
+        port = portCanvas == 0 ? port : portCanvas;
 
+        //creem el driver i l'intentem vincular a l'adre�a indicada
+        network_driver = NetworkDriver.Create();
+        connexio = default(NetworkConnection);
+        var direccio_server= NetworkEndpoint.Parse(ipAddress, port);
+        connexio = network_driver.Connect(direccio_server);
+        Debug.Log("CLIENT::conectant amb el port:" + direccio_server.Port + " i IP:" + direccio_server.Address);
+
+    }
     public void OnDestroy()
     {
-        conexion.Disconnect(net_driver);
-        conexion = default(NetworkConnection);
-        net_driver.Dispose();
-    }
-
-    private void CheckConexion()
-    {
-        if (!conexion.IsCreated)
-        {
-            Debug.Log("CLIENTE: no se ha podido conectar");
-        }
-    }
-
-    private void CheckEvents()
-    {
-        Unity.Collections.DataStreamReader stream_lectura;
-        NetworkEvent.Type net_evt_type = net_driver.PopEventForConnection(conexion, out stream_lectura);
-        while (net_evt_type != NetworkEvent.Type.Empty)
-        {
-            switch (net_evt_type)
-            {
-                case NetworkEvent.Type.Connect:
-                    Debug.Log("Cliente: conectado al servidor");
-                    // Instanciar el GameObject del jugador en el cliente
-                    GameObject player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
-                    break;
-            }
-            net_evt_type = net_driver.PopEventForConnection(conexion, out stream_lectura);
-        }
+        connexio.Disconnect(network_driver);
+        //A good pattern is to always set your NetworkConnection to
+        //default(NetworkConnection) to avoid stale references.
+        connexio = default(NetworkConnection); 
+        network_driver.Dispose();
     }
 
     void Update()
     {
-        net_driver.ScheduleUpdate().Complete();
-        CheckConexion();
-        CheckEvents();
+
+        //esperem a rebre tota la informaci� del driver
+       network_driver.ScheduleUpdate().Complete();
+        CheckConnexio(); //comprovem si encara estem conectats amb el servidor
+        LlegirMissatgesRebuts(); //actualitzem els missatges rebuts del servidor
+
+       
+    }
+    //d
+    private void FixedUpdate()
+    {
+        // Obtener la entrada del teclado para el movimiento
+        float mov_h = Input.GetAxis("Horizontal");
+        float mov_v = Input.GetAxis("Vertical");
+
+        // Calcular el vector de movimiento
+        Vector3 posicio = new Vector3(mov_h, mov_v, 0f) * velocitat ;
+        if (posicio_ultima != posicio)
+        {
+            // Aplicar el movimiento al GameObject
+            player.transform.Translate(posicio);
+            //avisa al servidor
+            SendMsgServer(new Missatge(id_key, "mou", JsonUtility.ToJson(player.transform.position)));
+        }
+    }
+    private void CheckConnexio()
+    {
+        if (!connexio.IsCreated)
+        {
+            Debug.Log("CLIENT:: error de conexio amb el servidor");
+        }
+    }
+    private void LlegirMissatgesRebuts()
+    {
+        // mirem els events que s'hi han produ�t
+         DataStreamReader stream_lectura;
+        NetworkEvent.Type net_event_type = network_driver.PopEventForConnection(connexio, out stream_lectura);
+        //mentres no retorni un event Empty, llegim el event
+        while (net_event_type != NetworkEvent.Type.Empty)
+        {
+            switch (net_event_type)
+            {
+                case NetworkEvent.Type.Connect:
+
+                    Debug.Log("CLIENT:: conectat amb el servidor");
+                    //dd streamEscritura.WriteFixedString128("Soc el client");
+                    SendMsgServer(new Missatge(-1, "inicia", ""));
+                    break;
+
+                case NetworkEvent.Type.Data:
+                    FixedString128Bytes text = stream_lectura.ReadFixedString128();
+                    Debug.Log("CLIENT rep:" + text);
+                    //podem escriure una resposta
+                    // network_driver.BeginSend(NetworkPipeline.Null, connexio, out var stream_escritura);
+                    // stream_escritura.WriteFixedString128("OK");
+                    // network_driver.EndSend(stream_escritura);
+                    Missatge mis = JsonUtility.FromJson<Missatge>(text.ToString());
+                    switch (mis.op)
+                    {
+                        case "inicia":
+                            id_key = mis.key;
+
+                            
+                            KeyClientList keysList = JsonUtility.FromJson<KeyClientList>(mis.msg);
+
+                            foreach (int key in keysList.Items)
+                            {
+                                if(key!= id_key)
+                                {
+                                    Player oponent = Instantiate(playerPrefab, new Vector3(3, 3), Quaternion.identity);
+                                    oponent.is_main_player = false;
+                                    oponents[key] = oponent;
+                                }
+                               
+                            }
+
+                                player = Instantiate(playerPrefab, new Vector3(1, 1), Quaternion.identity);
+                                player.is_main_player = true;
+                            break;
+                        case "nou_oponent":
+                            
+                            Player g1 = Instantiate(playerPrefab, new Vector3(3,3), Quaternion.identity);
+                            g1.is_main_player = false;
+                            oponents[mis.key] = g1;
+
+
+                            break;
+                        case "mou":
+                            oponents[mis.key].transform.position = ( JsonUtility.FromJson<Vector3>(mis.msg));
+                            break;
+                    }
+
+                    break;
+                case NetworkEvent.Type.Disconnect:
+                    Debug.Log("SERVIDOR: client desconectat");
+                    connexio = default(NetworkConnection);
+                    break;
+                default:
+                    Debug.Log("CLIENT event no controlat: " + net_event_type);
+                    break;
+            }
+            net_event_type = network_driver.PopEventForConnection(connexio, out stream_lectura);
+        }
+    }
+
+    public void SendChatMessage(string text)
+    {
+        network_driver.BeginSend(connexio, out var streamEscritura);
+        streamEscritura.WriteFixedString128("CLIENT: "+ text);
+        network_driver.EndSend(streamEscritura);
+    }
+    public void SendMsgServer(Missatge m)
+    {
+        network_driver.BeginSend(connexio, out var streamEscritura);
+        streamEscritura.WriteFixedString128(JsonUtility.ToJson(m));
+
+        network_driver.EndSend(streamEscritura);
     }
 }
-
--- Assets/Scripts/Network/Servidor.cs
+++ Assets/Scripts/Network/Servidor.cs
@@ -1,56 +1,224 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 using Unity.Networking.Transport;
 using Unity.Collections;
 
-public class Server : MonoBehaviour
+using UnityEngine;
+
+public class Servidor : MonoBehaviour
 {
+    public string ipAddress = "127.0.0.1";
     public ushort port = 9000;
-    public NetworkDriver net_driver;
-    private NetworkPipeline pipeline;
-    public GameObject playerPrefab;
-    private NetworkEndpoint serverEndpoint;
+
+//d
+public NetworkDriver network_driver;
+private NativeList<NetworkConnection> connexions;
+
+private Dictionary<int, NetworkConnection> connexions_dic = new Dictionary<int, NetworkConnection>();
+private int total_connexions = 0;
 
     void Start()
-    {
-        net_driver = NetworkDriver.Create();
-        serverEndpoint = NetworkEndpoint.AnyIpv4;
-        serverEndpoint.Port = port;
+{
+    string ipAddressCanvas = "127.0.0.1";
+    ushort.TryParse( "9000",  out var portCanvas);
+    ipAddress = string.IsNullOrEmpty(ipAddressCanvas)  ? ipAddress: ipAddressCanvas;
+    port = portCanvas == 0 ? port : portCanvas;
 
-        if (net_driver.Bind(serverEndpoint) != 0)
-        {
-            Debug.Log("Error al enlazar el servidor al puerto " + port);
-        }
-        else
-        {
-            net_driver.Listen();
-            Debug.Log("Servidor escuchando en el puerto " + port);
-        }
+    //creem el driver i l'intentem vincular a l'adre�a indicada
+    network_driver = NetworkDriver.Create();
+    var servidor_ws = NetworkEndpoint.Parse(ipAddress, port);
+    if (network_driver.Bind(servidor_ws) != 0)
+    {
+        Debug.Log("SERVIDOR::No ens hem pogut vincular amb el port:" + servidor_ws.Port+" i IP:"+ servidor_ws.Address);
     }
+    else
+    {
+        //iniciem la llista que emmagatzemi les conexions
+        connexions = new NativeList<NetworkConnection>(4, Allocator.Persistent);
+        //ens quedem a l'espera de noves conexion
+        network_driver.Listen();
+        Debug.Log("SERVIDOR:: a la espera en el port:" + servidor_ws.Port + " i IP:" + servidor_ws.Address);
+    }
+}
+    /*Quan tanquem el servidor hem d'alliberar la conexi� i la llista de conexions
+     */
 
     private void OnDestroy()
-    {
-        net_driver.Dispose();
+    {//si hem inicialitzat el driver, llavors l'esborrarem de la memoria junt amb la llista de conexions
+        if (network_driver.IsCreated)
+        {
+            network_driver.Dispose();
+            connexions.Dispose();
+        }
+        Debug.Log("SERVIDOR:: stop");
     }
+
 
     void Update()
     {
-        net_driver.ScheduleUpdate().Complete();
-
-        // Aceptar nuevas conexiones
-        NetworkConnection conexion;
-        while ((conexion = net_driver.Accept()) != default(NetworkConnection))
+        //esperem a rebre tota la informaci� del driver
+        network_driver.ScheduleUpdate().Complete();
+        RefrescarConnexions();
+        LlegirMissatgesRebuts();
+    }
+    /*Netejem connexions caducades i acceptem noves connexions*/
+    private void RefrescarConnexions()
+    {
+        //netejar connexions caducades
+        for(int i=0; i < connexions.Length; i++)
         {
-            Debug.Log("Servidor: nuevo cliente conectado");
+            if (!connexions[i].IsCreated)
+            {
+                connexions.RemoveAtSwapBack(i);
+                i--;
+            }
+        }
+        //acceptem noves connexions amb network_driver.Accept();
+        //retorna  default(NetworkConnection) si no hi han m�s connexions
+        NetworkConnection net_connection = network_driver.Accept();
+        while (net_connection != default(NetworkConnection))
+        {
+//total_connexions++;
+connexions_dic[++total_connexions] = net_connection;
+//d            connexions.Add(net_connection);
 
-            // Envía un mensaje de confirmación al cliente de que se ha conectado correctamente
-            // Esto puede ser necesario dependiendo de la lógica de tu juego
-            // Enviar mensaje de confirmación
-
-            // Instanciar el GameObject del jugador en el servidor
-            GameObject player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+            Debug.Log("SERVER:Nova connexio acceptada");
+            net_connection = network_driver.Accept(); //llegim la seguen connexio
         }
     }
+    /*Recorrem totes les conexions llegint els "NeworkEvents" que s'hagin pogut produir en aquella conexi�. 
+     * Els "events" que controlarem son:
+     *  "Data" -> ens indica que en la conexi� hem rebut informaci�
+     *  "Disconect" -> ens indica que el client s'ha desconectat
+     * */
+    public void LlegirMissatgesRebuts()
+    {
+
+//d       for (int k = 0; k < connexions.Length; k++)
+foreach (var kvp in connexions_dic)
+        
+        {
+            
+                        //per cada conexi� mirem els events que s'hi han produ�t
+                        //out permet pasar una variable per referencia (a diferencia de ref no es necesari que estigui declarada )
+                        DataStreamReader stream_lectura;
+
+//d            NetworkEvent.Type net_event_type = network_driver.PopEventForConnection(connexions[k], out stream_lectura);
+NetworkEvent.Type net_event_type = network_driver.PopEventForConnection(kvp.Value, out stream_lectura);
+
+            //mentres no retorni un event Empty, llegim el event
+            while (net_event_type != NetworkEvent.Type.Empty)
+            {
+                switch (net_event_type)
+                {
+                    case NetworkEvent.Type.Data:
+                        FixedString128Bytes text = stream_lectura.ReadFixedString128();
+                        Debug.Log("SERVIDOR rep:" + text);
+
+                        //podem escriure una resposta
+                        /* network_driver.BeginSend(NetworkPipeline.Null, connexions[k], out var stream_escritura);
+                         stream_escritura.WriteFixedString128("-"+ text);
+                         network_driver.EndSend(stream_escritura);*/
+
+//d                      BroadCast(text.ToString());
+Missatge mis = JsonUtility.FromJson<Missatge>(text.ToString());
+switch (mis.op)
+{
+
+    case "inicia":
+        Debug.Log("client  inicia");
+        KeyClientList key_ids = new KeyClientList(new List<int>(connexions_dic.Keys));
+                                
+        SendToOne(kvp.Value, new Missatge(kvp.Key, "inicia", JsonUtility.ToJson(key_ids)));
+                BroadCastOthers(kvp.Key, kvp.Value, new Missatge(kvp.Key, "nou_oponent", "s'ha iniciat un nou oponent"));
+        break;
+    case "mou":
+        //Debug.Log("client es mou");
+        BroadCastOthers(kvp.Key, kvp.Value, mis);
+        break;
+    default:
+        Debug.Log("accio del client no controlada en el servidor");
+        break;
+}
+
+
+                        break;
+                    case NetworkEvent.Type.Disconnect:
+                        Debug.Log("SERVIDOR: client desconectat");
+//d                        connexions[k] = default(NetworkConnection);
+connexions_dic.Remove(kvp.Key);
+                        break;
+                    default:
+                        Debug.Log("SERVIDOR event no controlat: " + net_event_type);
+                        break;
+                }
+//d net_event_type = network_driver.PopEventForConnection(connexions[k], out stream_lectura);
+net_event_type = network_driver.PopEventForConnection(kvp.Value, out stream_lectura);
+
+            }
+
+        }
+    }
+public virtual void BroadCastOthers(int key, NetworkConnection connection, Missatge missatge)
+{
+    foreach (var kvp in connexions_dic)
+    {
+        if (kvp.Value.IsCreated && kvp.Key != key)
+        {
+
+            string text = JsonUtility.ToJson(missatge);
+          //  Debug.Log("SERVER:broadcast:" + text);
+            network_driver.BeginSend(NetworkPipeline.Null, kvp.Value, out var stream_escritura);
+            stream_escritura.WriteFixedString128(text);
+            network_driver.EndSend(stream_escritura);
+        }
+    }
+}
+public virtual void SendToOne(NetworkConnection connection, Missatge missatge)
+{
+        Debug.Log("Send to One: " + missatge.msg);
+    string text = JsonUtility.ToJson(missatge);
+    network_driver.BeginSend(NetworkPipeline.Null, connection, out var stream_escritura);
+    stream_escritura.WriteFixedString128(text);
+    network_driver.EndSend(stream_escritura);
+}
+
+
+    public virtual void BroadCast(string text)
+    {
+        for (int i = 0; i < connexions.Length; i++)
+        {
+            if (connexions[i].IsCreated)
+            {
+                Debug.Log("SERVER:broadcast:" + text);
+                network_driver.BeginSend(NetworkPipeline.Null, connexions[i], out var stream_escritura);
+                stream_escritura.WriteFixedString128("-" + text);
+                network_driver.EndSend(stream_escritura);
+            }
+        }
+    }
+}
+[System.Serializable]
+public class Missatge
+{
+    public int key;
+    public string op;
+    public  string msg;
+    public Missatge(int key = -1, string op="", string msg ="")
+    {
+        this.key = key;
+        this.op = op;
+        this.msg = msg;
+    }
+}
+
+
+[System.Serializable]
+public class KeyClientList
+{    public KeyClientList(List<int> items)
+    {
+        this.Items = items;
+    }
+    public List<int> Items;
 }
 
